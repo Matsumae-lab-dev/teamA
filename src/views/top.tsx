2552f5bbf1a3dc3e80fad4a6a9ca66081fa31ebb
kanegon <prime.57@outlook.jp>
2023-12-14 18:06:14 +0900

    ログイン画面を追加

---
+++ src/views/top.tsx
@@ -0,0 +1,16 @@
+//Top.tsx
+import { useNavigate } from "react-router-dom";
+
+export default function Top() {
+    const navigate = useNavigate();
+    
+    return (
+        <div className="container">
+        <h1>Top Page</h1>
+        <button 
+            onClick={() => navigate('/')}
+            >ログアウト
+        </button>
+        </div>
+    );
+}
